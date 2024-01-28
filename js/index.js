fd25cd027239e5830c92ee500841f65cbe72459d
dilshod2604 <dilshodadilbekov456@gmail.com>
2024-01-28 20:33:22 +0600

    finished header burger button

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let header=document.querySelector('.header')
+let burgerBtn=document.querySelector('.header__burger_button')
+
+burgerBtn.addEventListener('click', ()=>{
+header.classList.toggle("open")
+})
